e9f166ab58f104f7d6e6234b4cb644560dc7f454
h2parson <haydenfparsons@icloud.com>
2026-08-10 11:56:31 -0700

    push

--- thrpt_lat.py
+++ thrpt_lat.py
@@ -6,7 +6,7 @@ CONFIG = "configs/cfg_Bonnell_common.txt"
 OUT_FILE = "nanobench_output.txt"
 # -----------------------------
 
-def test_single(asm, out_file):
+def test_single(asm):
     cmd = ["sudo", "./nanoBench.sh", "-asm", asm, "-config", CONFIG]
 
     result = subprocess.run(
@@ -28,12 +28,51 @@ def test_single(asm, out_file):
         elif line.startswith("INST_RETIRED:"):
             insts = line.split(" ")[1]
 
-    print(f"Cycles = {cycles}")
-    print(f"insts = {insts}")
+    return cycles, insts
+
+byte_regs =    ["AL", "BL", "CL", "DL"]
+word_regs =    ["AX", "BX", "CX", "DX"]
+dblword_regs = ["EAX", "EBX", "ECX", "EDX"]
+qdword_regs =  ["RAX", "RBX", "RCX", "RDX"]
+xmm_regs =     [f"XMM{i}" for i in range(16)]
+ymm_regs =     [f"YMM{i}" for i in range(16)]
+
+def n_regs(sz, n):
+    if sz == 'HW':
+        return byte_regs[:n]
+    elif sz == 'SW':
+        return word_regs[:n]
+    elif sz == 'DW':
+        return dblword_regs[:n]
+    elif sz == 'QW':
+        return qdword_regs[:n]
+    elif sz == 'X':
+        return xmm_regs[:n]
+    elif sz == 'Y':
+        return ymm_regs[:n]
+    return None
+
+
+def latency_single(asm_temp, sz):
+    args = n_regs(sz, asm_temp.count("{}"))
+    inst1 = asm_temp.format(*args)
+    inst2 = asm_temp.format(*args.reverse())
+    asm = inst1 + "; " + inst2
+
+    cycles, insts = test_single(asm)
+
+    latency = cycles / insts
+
+    print(f"latency = {latency}")
+
+    return latency
 
 
 def main():
-    test_single(ASM, OUT_FILE)
+    add_temp = r"ADD {0}, {1}"
+    sz = "QW"
+
+    latency_single(add_temp, sz)
 
     # with open(out_file, "w") as f:
     #         f.write(stdout)
