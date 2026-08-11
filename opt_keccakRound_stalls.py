c5d5aa44462aedcd334c1ce76c688bb83340e9f3
h2parson <haydenfparsons@icloud.com>
2026-08-11 07:19:20 -0700

    push

--- opt_keccakRound_stalls.py
+++ opt_keccakRound_stalls.py
@@ -1,13 +1,16 @@
 import subprocess
+import sys
 
 NANOBENCH_DIR = "../nanoBench" 
 CONFIG = "configs/cfg_Bonnell_common.txt"
 # -----------------------------
 
-def main():
+def main(unroll, loops):
     with open('./keccakRound/opt_formatted.s', "r", encoding="utf-8") as asm:
         asm_code = asm.read()
-        cmd = ["sudo", "./nanoBench.sh", "-asm", asm_code, "-config", CONFIG]
+        unroll_txt = ("-unroll_count=" + unroll) if unroll else ""
+        loops_txt = ("-loop_count=" + loops) if loops else ""
+        cmd = ["sudo", "./nanoBench.sh", "-asm", asm_code, "-config", CONFIG, unroll_txt, loops_txt]
 
         result = subprocess.run(
             cmd,
@@ -21,4 +24,4 @@ def main():
     print(output)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1],sys.argv[2])
