ed7727bf9057d3864086f7f84f4fb6633b68b809
h2parson <haydenfparsons@icloud.com>
2026-08-13 10:53:32 -0700

    push

--- section_test.py
+++ section_test.py
@@ -21,8 +21,8 @@ def main(path, start, end):
     # print out the results
     with open("temp1.s", "r", encoding="utf-8") as asm:
         asm_code = asm.read()
-        unroll_txt = "-unroll_count=1"
-        cmd = ["sudo", "./nanoBench.sh", "-asm", asm_code, "-config", CONFIG, unroll_txt, '-loop_count=1']
+        unroll_txt = "-unroll_count=2"
+        cmd = ["sudo", "./nanoBench.sh", "-asm", asm_code, "-config", CONFIG, unroll_txt]
 
         result = subprocess.run(
                     cmd,
