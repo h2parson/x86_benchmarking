6b2e9fde7fe2d522d246c99da86c74bba831ff7b
h2parson <haydenfparsons@icloud.com>
2026-08-18 15:14:46 -0400

    push

--- test.s
+++ test.s
@@ -1,3 +1,36 @@
-mov %rbx, %rax
-mov 0(%rdi), %r13
-mov %rsi, 0(%rsp)
+xor 64(%rdi), %r8
+mov %rbp, %rbx
+mov 24(%rdi), %r13
+mov %rsi, %r9
+xor 144(%rdi), %r8
+xor 104(%rdi), %r13
+mov $0x0, %r14
+xor %r8, %r13
+rol %rbx
+mov %r15, %r8
+mov 0(%rdi), %r10
+xor 56(%rdi), %rdx
+xor %r15, %rbx
+xor %rbx, %r10
+mov 16(%rdi), %rax
+xor 136(%rdi), %rdx
+xor %r10, %r14
+mov %r10, %r15
+xor 96(%rdi), %rax
+xor %rdx, %rax
+mov 144(%rdi), %r11
+mov %r13, %rdx
+rol %r8
+rol %rdx
+mov %rax, %rcx
+xor %rbp, %rdx
+xor %rax, %r8
+rol %r9
+xor %r8, %r11
+rol %rcx
+xor %r13, %r9
+rol $21, %r11
+xor %rsi, %rcx
+mov 48(%rdi), %r12
+mov %r11, %rbp
+mov 96(%rdi), %rax
